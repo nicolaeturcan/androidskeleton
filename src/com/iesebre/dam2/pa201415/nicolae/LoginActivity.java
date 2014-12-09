5fbae3981c769d4cffaee0f432e35d345a71c495
nicolae <nicolaeturcan@iesebre.com>
2014-12-09 14:02:44 +0100

    updateUI added

--- src/com/iesebre/dam2/pa201415/nicolae/LoginActivity.java
+++ src/com/iesebre/dam2/pa201415/nicolae/LoginActivity.java
@@ -29,64 +29,65 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity implements OnClickListener,ConnectionCallbacks,OnConnectionFailedListener {
-	
+public class LoginActivity extends Activity implements OnClickListener,
+		ConnectionCallbacks, OnConnectionFailedListener {
+
 	// Google client to interact with Google API
 	private GoogleApiClient mGoogleApiClient;
-	
+
 	private static final int RC_SIGN_IN_GOOGLE = 3889;
-	
+
 	private static final String TAG = "LoginActivity";
-	
+
 	private boolean mSignInClicked;
-	
+
 	// Profile pic image size in pixels
 	private static final int PROFILE_PIC_SIZE = 400;
 
 	private static final int REQUEST_CODE_GOOGLE_LOGIN = 91;
 	private static final int REQUEST_CODE_FACEBOOK_LOGIN = 92;
 	private static final int REQUEST_CODE_TWITTER_LOGIN = 93;
-	
+
 	private static final int SOCIAL_LOGIN_GOOGLE = 101;
 	private static final int SOCIAL_LOGIN_FACEBOOk = 102;
 	private static final int SOCIAL_LOGIN_TWITTER = 103;
-	
+
 	private boolean OnStartAlreadyConnected = false;
-	
-	private int socialLoginType=-1;
-	
+
+	private int socialLoginType = -1;
+
 	/**
 	 * A flag indicating that a PendingIntent is in progress and prevents us
 	 * from starting further intents.
 	 */
 	private boolean mIntentInProgress;
-	
+
 	private ConnectionResult mConnectionResult;
-	
+
 	private Button btnGoogleSignIn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Log.d(TAG,"onCreate!");
+		Log.d(TAG, "onCreate!");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
-		
+
 		btnGoogleSignIn = (Button) findViewById(R.id.btn_google_sign_in);
-		
+
 		// Button click listeners
 		btnGoogleSignIn.setOnClickListener(this);
-		
+
 		mGoogleApiClient = new GoogleApiClient.Builder(this)
-		.addConnectionCallbacks(this)
-		.addOnConnectionFailedListener(this).addApi(Plus.API)
-		.addScope(Plus.SCOPE_PLUS_LOGIN).build();
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).addApi(Plus.API)
+				.addScope(Plus.SCOPE_PLUS_LOGIN).build();
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int responseCode,
 			Intent intent) {
 		Log.d(TAG, "onActivityResult!");
-		
+
 		switch (requestCode) {
 		case RC_SIGN_IN_GOOGLE:
 			if (responseCode != RESULT_OK) {
@@ -102,16 +103,17 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 
 		case REQUEST_CODE_GOOGLE_LOGIN:
 			Log.d(TAG, "LOGOUT from google!");
-			//LOGOUT from google
+			// LOGOUT from google
 			if (responseCode == RESULT_OK) {
-				Log.d(TAG, "LOGOUT from google withe response code RESULT_OK. Signing Out from Gplus!...");
-				//Check if revoke exists!
+				Log.d(TAG,
+						"LOGOUT from google withe response code RESULT_OK. Signing Out from Gplus!...");
+				// Check if revoke exists!
 				Bundle extras = intent.getExtras();
 				boolean revoke = false;
 				if (extras != null) {
 					revoke = extras.getBoolean(AndroidSkeletonUtils.REVOKE_KEY);
 				}
-				
+
 				if (revoke == true) {
 					Log.d(TAG, "LOGOUT and also revoke!...");
 					revokeGplusAccess();
@@ -124,33 +126,34 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 		default:
 			break;
 		}
-		
-		
+
 	}
-	
+
 	protected void onStart() {
 		Log.d(TAG, "onStart!");
 		super.onStart();
-		
+
 		if (!this.OnStartAlreadyConnected) {
-			//NOT ALREADY CONNECTED: CONNECT!
+			// NOT ALREADY CONNECTED: CONNECT!
 			Log.d(TAG, "NOT Already connected! Connecting to...");
 			mGoogleApiClient.connect();
 			Log.d(TAG, "...connected to Google API!");
-		} else{
+		} else {
 			Log.d(TAG, "Already connected!");
 		}
-		
+
 	}
 
 	protected void onStop() {
 		Log.d(TAG, "onStop!");
 		super.onStop();
-		/* NOT DISCONNECT! WE will come back after result and OnActivityResult does not executes previously onStart!
-		 
-			Log.d(TAG, "onStop mGoogleApiClient isConnected. Disconnecting...!");
-			mGoogleApiClient.disconnect();
-		}*/
+		/*
+		 * NOT DISCONNECT! WE will come back after result and OnActivityResult
+		 * does not executes previously onStart!
+		 * 
+		 * Log.d(TAG, "onStop mGoogleApiClient isConnected. Disconnecting...!");
+		 * mGoogleApiClient.disconnect(); }
+		 */
 	}
 
 	@Override
@@ -159,7 +162,7 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	/**
 	 * Revoking access from google
 	 * */
@@ -173,12 +176,13 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 							Log.e(TAG, "User access revoked!");
 							mGoogleApiClient.disconnect();
 							mGoogleApiClient.connect();
+							updateUI(false);
 						}
 
 					});
 		}
 	}
-	
+
 	/**
 	 * Sign-in into google
 	 * */
@@ -199,18 +203,14 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 			// Signin button clicked
 			signInWithGplus();
 			break;
-			/*
-		case R.id.btn_sign_out:
-			// Signout button clicked
-			signOutFromGplus();
-			break;
-		case R.id.btn_revoke_access:
-			// Revoke access button clicked
-			revokeGplusAccess();
-			break;*/
+		/*
+		 * case R.id.btn_sign_out: // Signout button clicked signOutFromGplus();
+		 * break; case R.id.btn_revoke_access: // Revoke access button clicked
+		 * revokeGplusAccess(); break;
+		 */
 		}
 	}
-	
+
 	/**
 	 * Sign-out from google
 	 * */
@@ -221,6 +221,7 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 			Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
 			mGoogleApiClient.disconnect();
 			mGoogleApiClient.connect();
+			updateUI(false);
 		}
 	}
 
@@ -228,25 +229,25 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 	public void onConnected(Bundle connectionHint) {
 		Log.d(TAG, "onConnected!");
 		// TODO Auto-generated method stub
-		
+
 		mSignInClicked = false;
-		
-		//Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+
+		// Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
 		Log.d(TAG, "User is connected!");
-		
-		this.socialLoginType=SOCIAL_LOGIN_GOOGLE;
+
+		this.socialLoginType = SOCIAL_LOGIN_GOOGLE;
 
 		// Get user's information
 		getProfileInformation();
 
 		Intent i = new Intent(LoginActivity.this, MainActivity.class);
 		startActivityForResult(i, REQUEST_CODE_GOOGLE_LOGIN);
-		
+
 		// Update the UI after signin
-		//updateUI(true);
-		
+		updateUI(true);
+
 	}
-	
+
 	/**
 	 * Fetching user's information name, email, profile pic
 	 * */
@@ -264,8 +265,7 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 						+ personGooglePlusProfile + ", email: " + email
 						+ ", Image: " + personPhotoUrl);
 
-				//TODO: Save to shared preferences?
-			
+				// TODO: Save to shared preferences?
 
 				// by default the profile url gives 50x50 px image only
 				// we can replace the value with whatever dimension we want by
@@ -274,7 +274,7 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 						personPhotoUrl.length() - 2)
 						+ PROFILE_PIC_SIZE;
 
-				//new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
+				// new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
 
 			} else {
 				Toast.makeText(getApplicationContext(),
@@ -285,11 +285,31 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 		}
 	}
 
+	/**
+	 * Updating the UI, showing/hiding buttons and profile layout ACTIONS TO DO
+	 * ONCE WE ARE SIGN IN
+	 * */
+	private void updateUI(boolean isSignedIn) {
+		if (isSignedIn) {
+
+			// logging with Google+ and going to NEXT ACTIVITY.
+			Intent googleLogin = new Intent(LoginActivity.this,
+					MainActivity.class);
+			startActivityForResult(googleLogin, REQUEST_CODE_GOOGLE_LOGIN);
+
+		} /*
+		 * else { //if false we don't need to do nothing Toast.makeText(context,
+		 * "User Disconnected from Google+", Toast.LENGTH_LONG).show(); return;
+		 * 
+		 * }
+		 */
+	}
+
 	@Override
 	public void onConnectionSuspended(int cause) {
 		// TODO Auto-generated method stub
 		mGoogleApiClient.connect();
-		//updateUI(false);
+		updateUI(false);
 	}
 
 	@Override
@@ -312,16 +332,17 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 			}
 		}
 	}
-	
+
 	/**
 	 * Method to resolve any signin errors
 	 * */
 	private void resolveSignInError() {
-		if (mConnectionResult!=null){
+		if (mConnectionResult != null) {
 			if (mConnectionResult.hasResolution()) {
 				try {
 					mIntentInProgress = true;
-					mConnectionResult.startResolutionForResult(this, RC_SIGN_IN_GOOGLE);
+					mConnectionResult.startResolutionForResult(this,
+							RC_SIGN_IN_GOOGLE);
 				} catch (SendIntentException e) {
 					mIntentInProgress = false;
 					mGoogleApiClient.connect();
@@ -329,7 +350,7 @@ public class LoginActivity extends Activity implements OnClickListener,Connectio
 			}
 		}
 	}
-	
+
 	/**
 	 * Background Async task to load user profile picture from url
 	 * */
